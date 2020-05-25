e4d916c12564d22ffab01bb2047d444f460df3d1
Zaid Akhter <zaidakhter1202@gmail.com>
2020-05-25 18:35:34 +0500

    📦 NEW: Add Dynamic routes for posts

---
+++ pages/posts/[id].js
@@ -0,0 +1,32 @@
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Layout from "../../components/layout";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
